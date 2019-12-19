41cdd15fc74c0050b8b50fe57eca1fc56acb154c
Aram Galarza Roca <aramgr@TTC.CAT>
2019-12-19 11:38:22 +0100

    First commit

---
+++ Git1/Source.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+
+	printf("Witt says HI");
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+// git clone https:......
